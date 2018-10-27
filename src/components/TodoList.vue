e51f30f4c51f8e5a2993b02b170ad820fc3cc5f4
Lincoln Brito <lincoln.sbrito@gmail.com>
2018-10-27 13:05:47 -0300

    input todo

--- src/components/TodoList.vue
+++ src/components/TodoList.vue
@@ -1,5 +1,6 @@
 <template>
   <div>
+    <input type="text" class="todo-input" placeholder="What needs to be done">
     Todo list goes here
   </div>
 </template>
@@ -16,6 +17,15 @@ export default {
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
-<style>
+<style lang="scss">
+  .todo-input {
+    width: 100%;
+    padding: 10px 18px;
+    font-size: 18px;
+    margin-bottom: 16px;
 
+    &:focus{
+      outline: 0;
+    }
+  }
 </style>
