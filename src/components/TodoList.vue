4f119a160646d3e6837d94f647628b7edfe5b949
Lincoln Brito <lincoln.sbrito@gmail.com>
2018-10-27 13:53:44 -0300

    avoid edit with empty value

--- src/components/TodoList.vue
+++ src/components/TodoList.vue
@@ -59,6 +59,9 @@ export default {
     },
 
     doneEdit(todo) {
+      if(todo.title.trim().length == 0){
+        todo.title = this.beforeEditCache;
+      }
       todo.editing = false;
     },
 
