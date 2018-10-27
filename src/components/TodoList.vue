7ab441279a8a1821d72595bb2374d734f42215cd
Lincoln Brito <lincoln.sbrito@gmail.com>
2018-10-27 13:13:08 -0300

    addTodo method

--- src/components/TodoList.vue
+++ src/components/TodoList.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <input type="text" class="todo-input" placeholder="What needs to be done" v-model="newTodo">
+    <input type="text" class="todo-input" placeholder="What needs to be done" v-model="newTodo" @keyup.enter="addTodo">
     <div v-for="todo in todos" :key="todo.id" class="todo-item">
       {{ todo.title }}
     </div>
@@ -13,6 +13,7 @@ export default {
   data () {
     return {
       newTodo: '',
+      idForTodo: 3,
       todos: [
         {
           'id': 1,
@@ -25,6 +26,18 @@ export default {
           'completed': false
         }
       ]
+    }
+  },
+  methods: {
+    addTodo() {
+      this.todos.push({
+        id: this.idForTodo,
+        title: this.newTodo,
+        completed: false
+      });
+
+      this.newTodo = '';
+      this.idForTodo++;
     }
   }
 }
