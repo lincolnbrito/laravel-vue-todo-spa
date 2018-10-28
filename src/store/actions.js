3d5e2fd1f5f971a8e98b88b4f47d6e97be9a2bb7
Lincoln Brito <lincoln.sbrito@gmail.com>
2018-10-28 00:10:47 -0300

    split store in files

---
+++ src/store/actions.js
@@ -0,0 +1,33 @@
+export default {
+  addTodo(context, todo) {
+    setTimeout( () => {
+      context.commit('addTodo', todo)
+    }, 2000);
+
+  },
+  updateTodo(context, todo) {
+    setTimeout( () => {
+      context.commit('updateTodo', todo)
+    }, 2000);
+  },
+  deleteTodo(context, id) {
+    setTimeout( () => {
+      context.commit('deleteTodo', id)
+    }, 2000);
+  },
+  checkAll(context, checked) {
+    setTimeout( () => {
+      context.commit('checkAll', checked)
+    }, 2000);
+  },
+  updateFilter(context, filter){
+    setTimeout( () => {
+      context.commit('updateFilter', filter)
+    }, 2000);
+  },
+  clearCompleted(context) {
+    setTimeout( () => {
+      context.commit('clearCompleted')
+    }, 2000);
+  },
+}
--- src/store/store.js
+++ src/store/store.js
@@ -1,112 +1,15 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import state from './state'
+import getters from './getters'
+import mutations from './mutations'
 
 Vue.use(Vuex)
 
 export const store = new Vuex.Store({
-  state: {
-    filter: 'all',
-    todos: [
-      {
-        'id': 1,
-        'title': 'Finish Vue screencast',
-        'completed': false,
-        'editing': false
-      },
-      {
-        'id': 2,
-        'title': 'Take over world',
-        'completed': false,
-        'editing': false
-      }
-    ]
-  },
-  getters: {
-    remaining(state) {
-      return state.todos.filter( todo => !todo.completed).length;
-    },
-    anyRemaining(state, getters) {
-      return getters.remaining != 0;
-    },
-    todosFiltered(state) {
-      if(state.filter == 'all'){
-        return state.todos;
-      } else if(state.filter=='active') {
-        return state.todos.filter( todo => !todo.completed );
-      } else if(state.filter=='completed') {
-        return state.todos.filter( todo => todo.completed );
-      }
-
-      return state.todos;
-    },
-    showClearCompletedButton(state) {
-      return state.todos.filter( todo => todo.completed ).length > 0;
-    }
-  },
-  mutations: {
-    addTodo(state, todo) {
-      state.todos.push({
-        id: todo.id,
-        title: todo.title,
-        completed: false,
-        editing: false
-      })
-    },
-    updateTodo(state, todo) {
-      const index = state.todos.findIndex(item => item.id == todo.id );
-      state.todos.splice(index, 1, {
-        'id': todo.id,
-        'title': todo.title,
-        'completed': todo.completed,
-        'editing': todo.editing
-      })
-    },
-    deleteTodo(state, id) {
-      const index = state.todos.findIndex( item => item.id == id);
-      state.todos.splice(index, 1);
-    },
-    checkAll(state, checked) {
-      state.todos.forEach( todo => (todo.completed = checked));
-    },
-    updateFilter(state, filter){
-      state.filter = filter;
-    },
-    clearCompleted(state) {
-      state.todos = state.todos.filter(todo => !todo.completed);
-    },
-  },
-  actions: {
-    addTodo(context, todo) {
-      setTimeout( () => {
-        context.commit('addTodo', todo)
-      }, 2000);
-
-    },
-    updateTodo(context, todo) {
-      setTimeout( () => {
-        context.commit('updateTodo', todo)
-      }, 2000);
-    },
-    deleteTodo(context, id) {
-      setTimeout( () => {
-        context.commit('deleteTodo', id)
-      }, 2000);
-    },
-    checkAll(context, checked) {
-      setTimeout( () => {
-        context.commit('checkAll', checked)
-      }, 2000);
-    },
-    updateFilter(context, filter){
-      setTimeout( () => {
-        context.commit('updateFilter', filter)
-      }, 2000);
-    },
-    clearCompleted(context) {
-      setTimeout( () => {
-        context.commit('clearCompleted')
-      }, 2000);
-    },
-  }
+  state,
+  getters,
+  mutations,
+  actions
 })
 
