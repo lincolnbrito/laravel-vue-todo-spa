87cd21cf44ef3c48134f90f42d83e99f5ef10440
Lincoln Brito <lincoln.sbrito@gmail.com>
2018-10-30 17:12:12 -0300

    install and configure VueRouter

--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
+import VueRouter from 'vue-router'
 // import App from './App'
 import Master from './components/layouts/Master'
 import {store} from './store/store'
@@ -8,6 +9,7 @@ import {store} from './store/store'
 window.eventBus = new Vue()
 
 Vue.config.productionTip = false
+Vue.use(VueRouter)
 
 /* eslint-disable no-new */
 new Vue({
