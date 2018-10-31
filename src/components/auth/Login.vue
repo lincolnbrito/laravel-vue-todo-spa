8b24372e4eb98b50e03c31c0e81cea105a53ccd4
Lincoln Brito <lincoln.sbrito@gmail.com>
2018-10-31 09:46:47 -0300

    style login

--- src/components/auth/Login.vue
+++ src/components/auth/Login.vue
@@ -1,5 +1,28 @@
 <template>
-  <div class="flex-center">
-    content for login
+  <div class="login-form">
+    <h2 class="login-heading">Login</h2>
+    <form action="">
+
+      <div class="form-control">
+        <label for="email">Username/Email</label>
+        <input type="email" name="username" id="username" class="login-input">
+      </div>
+
+      <div class="form-control mb-more">
+        <label for="password">Password</label>
+        <input type="password" name="password" id="password" class="login-input">
+      </div>
+
+      <div class="form-control">
+        <button type="submit" class="btn-submit">Login</button>
+      </div>
+
+    </form>
   </div>
 </template>
+
+<script>
+  export default {
+
+  }
+</script>
