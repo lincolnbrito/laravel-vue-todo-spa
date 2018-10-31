9468a5f990ea2ab671b0dca7af136a97f7bf9b6d
Lincoln Brito <lincoln.sbrito@gmail.com>
2018-10-31 09:37:59 -0300

    change api baseurl

--- src/config/index.js
+++ src/config/index.js
@@ -1,5 +1,5 @@
 export default {
   'axios' : {
-    'baseURL': (process.env.NODE_ENV == 'development') ? 'http://localhost:8000/api' : process.env.API_BASE_URL
+    'baseURL': (process.env.NODE_ENV == 'development') ? 'http://laravel-vue-todo-api.local/api' : process.env.API_BASE_URL
   }
 }
